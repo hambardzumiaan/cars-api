3fe64ae4cc5dfc414919c0fff76f9ae4689577a1
hambardzumyan <hambardzumyan.bakur@gmail.com>
2023-03-25 20:26:03 +0400

    To greater from

--- app/Http/Requests/DeliveryPlanRequest.php
+++ app/Http/Requests/DeliveryPlanRequest.php
@@ -7,7 +7,7 @@ class DeliveryPlanRequest extends GeneralRequest
     public function rules() {
         return [
             'from' => 'required|integer',
-            'to' => 'required|integer',
+            'to' => 'required|integer|gt:from',
             'mile_price' => 'required|integer',
             'services_price' => 'required|integer',
             'additional_expenses' => 'required|integer',
